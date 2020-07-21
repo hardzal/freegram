cbc6a003a62773cd7ad617d0576377dc55b8c177
M Rizal <suryadijogja@gmail.com>
2020-07-21 21:25:34 +0700

    #0 - Migrate User and Make Auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Freegram
                 </div>
 
                 <div class="links">
