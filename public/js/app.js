0ebe7a82e53ad2e70754415f23ae70d03c5677b2
M Rizal <suryadijogja@gmail.com>
2020-07-21 22:35:25 +0700

    #1 - Add username column and style

--- public/js/app.js
+++ public/js/app.js
@@ -49888,8 +49888,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! c:\wamp64\www\latihan\laravel\freegram\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! c:\wamp64\www\latihan\laravel\freegram\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\wamp64\www\latihan\laravel\freegram\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\wamp64\www\latihan\laravel\freegram\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,58 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+   <div class="row">
+       <div class="col-md-3 p-5">
+            <img src='{{ url('images/prof.jpg')}}' class="rounded-circle"/>
+       </div>
+       <div class="col-md-9">
+           <div class="row">
+                <div class="col-md-4">
+                    <h1>@aimin.official</h1>
                 </div>
-            </div>
-        </div>
-    </div>
+                <div class="col-md-6">
+                    <button>Message</button>
+                    <button>Follow</button>
+                    <button>Related</button>
+                    <button>Option</button>
+                </div>
+           </div>
+           <div class="row">
+                <div class="col-md-4">
+                    <strong>100</strong> Posts
+                </div>
+                <div class="col-md-4">
+                    <strong>500</strong> Followers
+                </div>
+                <div class="col-md-4">
+                    <strong>5</strong> Following
+                </div>
+           </div>
+           <div class="row">
+               <div class="col">
+                    <p>Caption here</p>
+               </div>
+           </div>
+           <div class="row">
+               <div class="col"><a href="#">https://url.id</a></div>
+           </div>
+       </div>
+
+   </div>
+   <div class="d-flex justify-content-center">
+        <div class="px-5">Posts</div>
+        <div class="px-5">Tagged</div>
+   </div><hr/>
+   <div class="row pt-5">
+       <div class="col-md-4">
+            <img src="{{ url('images/posts/aimin_1.jpg') }}" alt="" class="w-100"/>
+       </div>
+       <div class="col-md-4">
+            <img src="{{ url('images/posts/aimin_1.jpg') }}" alt="" class="w-100"/>
+       </div>
+       <div class="col-md-4">
+            <img src="{{ url('images/posts/aimin_1.jpg') }}" alt="" class="w-100"/>
+       </div>
+   </div>
 </div>
 @endsection
